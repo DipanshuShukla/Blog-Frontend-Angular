d38762a5a642961dabb8b3f7bf59c8cae49c0478
DipanshuShukla <shuklascientist5@gmail.com>
2023-11-08 14:30:30 +0530

    fixed navbar

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 
 const HOSTNAME: string = 'localhost'
 const PORT_NUMBER: number = 5420
-const URL: string = 'http://' + HOSTNAME + ':' + PORT_NUMBER
-// const URL: string = 'https://nordicrosebackend.dipanshushukla.com'
+// const URL: string = 'http://' + HOSTNAME + ':' + PORT_NUMBER
+const URL: string = 'https://nordicrosebackend.dipanshushukla.com'
 
 export const environment = {
     production: false,
